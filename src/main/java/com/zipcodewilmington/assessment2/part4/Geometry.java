e62b2b590dcc6a2cd7f7602a17fa4a5cd1488e0b
Carolynn Vansant <cvansant@MacBook-Pro-5.local>
2018-03-05 11:17:02 -0500

    finished working on part4

--- src/main/java/com/zipcodewilmington/assessment2/part4/Geometry.java
+++ src/main/java/com/zipcodewilmington/assessment2/part4/Geometry.java
@@ -1,4 +1,43 @@
 package com.zipcodewilmington.assessment2.part4;
 
-public class Geometry {
+public class Geometry implements Circle, Rectangle {
+
+    private int height;
+    private int width;
+    private int radius;
+
+    Geometry(){
+        this.height = 0;
+        this.width = 0;
+        this.radius = 0;
+    }
+
+    Geometry(int height, int width, int radius) {
+        this.height = height;
+        this.width = width;
+        this.radius = radius;
+    }
+
+    public int getArea(int height, int width) {
+        int rectangleArea = this.height * this.width;
+        return rectangleArea;
+    }
+
+    public double getArea(int radius) {
+        double circleArea = this.radius * this.radius * 3.14;
+        return circleArea;
+    }
+
+
+    @Override
+    public double area(int radius) {
+        double circleArea = this.radius * this.radius * 3.14;
+        return circleArea;
+    }
+
+    @Override
+    public int area(int height, int width) {
+        int rectangleArea = this.height * this.width;
+        return rectangleArea;
+    }
 }
